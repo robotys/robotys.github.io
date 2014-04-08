5764b7a0733b059ee0401e4cc34dc88b5a57756c
robotys <izwanwahab@pts.com.my>
2014-04-08 12:23:15 +0800

    little progress

---
+++ gen.php
@@ -0,0 +1,22 @@
+<?php
+	
+	// we can be sloppy for these phase. What phase? RESEARCH & PROTOTYPING phase ow yeah!
+
+	// we need to generate
+
+	// from mds
+
+	// everything that has YYYY-MM-DD in filename is good to go.
+
+	// the index array structure should be (but not comfirmed yet) as below:
+
+	$index = array(
+					'slug-after-hashbang'=>'timestamp gmt',
+					'slug-after-hashbang'=>13456993454,
+					'slug-after-hashbang'=>13456993454,
+					'slug-after-hashbang'=>13456993454
+				);
+
+	// in json mind you. :)
+
+?>
